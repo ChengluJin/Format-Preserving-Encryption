8631efac7604e18c73d18f10a6c389f6fd9d10b6
ChengluJin <jin.cheng.lu.china@gmail.com>
2020-06-12 01:35:36 -0400

    measurement

--- example.c
+++ example.c
@@ -3,122 +3,139 @@
 #include <assert.h>
 #include <string.h>
 #include <fpe.h>
+#include <time.h>
 
 void hex2chars(unsigned char hex[], unsigned char result[])
 {
-    int len = strlen(hex);
-    unsigned char temp[3];
-    temp[2] = 0x00;
+	int len = strlen(hex);
+	unsigned char temp[3];
+	temp[2] = 0x00;
 
-    int j = 0;
-    for (int i = 0; i < len; i += 2) {
-        temp[0] = hex[i];
-        temp[1] = hex[i + 1];
-        result[j] = (char)strtol(temp, NULL, 16);
-        ++j;
-    }
+	int j = 0;
+	for (int i = 0; i < len; i += 2) {
+		temp[0] = hex[i];
+		temp[1] = hex[i + 1];
+		result[j] = (char)strtol(temp, NULL, 16);
+		++j;
+	}
 }
 
 void map_chars(unsigned char str[], unsigned int result[])
 {
-    int len = strlen(str);
+	int len = strlen(str);
 
-    for (int i = 0; i < len; ++i)
-        if (str[i] >= 'a')
-            result[i] = str[i] - 'a' + 10;
-        else
-            result[i] = str[i] - '0';
+	for (int i = 0; i < len; ++i)
+		if (str[i] >= 'a')
+			result[i] = str[i] - 'a' + 10;
+		else
+			result[i] = str[i] - '0';
 }
 
 void inverse_map_chars(unsigned result[], unsigned char str[], int len)
 {
-    for (int i = 0; i < len; ++i)
-        if (result[i] < 10)
-            str[i] = result[i] + '0';
-        else 
-            str[i] = result[i] - 10 + 'a';
+	for (int i = 0; i < len; ++i)
+		if (result[i] < 10)
+			str[i] = result[i] + '0';
+		else 
+			str[i] = result[i] - 10 + 'a';
 
-    str[len] = 0x00;
+	str[len] = 0x00;
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 5) {
-        printf("Usage: %s <key> <tweak> <radix> <plaintext>\n", argv[0]);
-        return 0;
-    }
+	if (argc != 5) {
+		printf("Usage: %s <key> <tweak> <radix> <plaintext>\n", argv[0]);
+		return 0;
+	}
 
-    unsigned char k[100],
-                  t[100],
-                  result[100];
-    int xlen = strlen(argv[4]),
-        klen = strlen(argv[1]) / 2,
-        tlen = strlen(argv[2]) / 2,
-        radix = atoi(argv[3]);
-    unsigned int x[100],
-                 y[xlen];
-    unsigned int tmp;
+	unsigned char k[200],
+	t[100],
+	result[200];
+	int xlen = strlen(argv[4]),
+	    klen = strlen(argv[1]) / 2,
+	    tlen = strlen(argv[2]) / 2,
+	    radix = atoi(argv[3]);
+	unsigned int x[200],
+	y[xlen];
+	unsigned int tmp;
 
-    hex2chars(argv[1], k);
-    hex2chars(argv[2], t);
-    map_chars(argv[4], x);
+	hex2chars(argv[1], k);
+	hex2chars(argv[2], t);
+	map_chars(argv[4], x);
 
-    for (int i = 0; i < xlen; ++i)
-        assert(x[i] < radix);
+	for (int i = 0; i < xlen; ++i)
+		assert(x[i] < radix);
 
-    FPE_KEY ff1, ff3;
+	FPE_KEY ff1, ff3;
 
-    printf("key:");
-    for (int i = 0; i < klen; ++i)    printf(" %02x", k[i]);
-    puts("");
-    if (tlen)    printf("tweak:");
-    for (int i = 0; i < tlen; ++i)    printf(" %02x", t[i]);
-    if (tlen)    puts("");
+	printf("key:");
+	for (int i = 0; i < klen; ++i)    printf(" %02x", k[i]);
+	puts("");
+	if (tlen)    printf("tweak:");
+	for (int i = 0; i < tlen; ++i)    printf(" %02x", t[i]);
+	if (tlen)    puts("");
 
-    FPE_set_ff1_key(k, klen * 8, t, tlen, radix, &ff1);
-    FPE_set_ff3_key(k, klen * 8, t, radix, &ff3);
+	FPE_set_ff1_key(k, klen * 8, t, tlen, radix, &ff1);
+	FPE_set_ff3_key(k, klen * 8, t, radix, &ff3);
 
-    printf("after map: ");
-    for (int i = 0; i < xlen; ++i)    printf(" %d", x[i]);
-    printf("\n\n");
+	printf("after map: ");
+	for (int i = 0; i < xlen; ++i)    printf(" %d", x[i]);
+	printf("\n\n");
 
-    printf("========== FF1 ==========\n");
-    FPE_ff1_encrypt(x, y, xlen, &ff1, FPE_ENCRYPT);
+	printf("========== FF1 ==========\n");
+	clock_t start_enc = clock();
+	for (int loop_index = 0 ; loop_index < 100; loop_index++)
+	{
+		FPE_ff1_encrypt(x, y, xlen, &ff1, FPE_ENCRYPT);
+	}
+	clock_t end_enc = clock();
 
-    printf("ciphertext(numeral string):");
-    for (int i = 0; i < xlen; ++i)    printf(" %d", y[i]);
-    printf("\n");
+	double elapsed_secs = ((double)(end_enc - start_enc)) / CLOCKS_PER_SEC;
+	printf("Enc = %lf\n", elapsed_secs);
 
-    inverse_map_chars(y, result, xlen);
-    printf("ciphertext: %s\n\n", result);
+	printf("ciphertext(numeral string):");
+	for (int i = 0; i < xlen; ++i)    printf(" %d", y[i]);
+	printf("\n");
 
-    memset(x, 0, sizeof(x));
-    FPE_ff1_encrypt(y, x, xlen, &ff1, FPE_DECRYPT);
+	inverse_map_chars(y, result, xlen);
+	printf("ciphertext: %s\n\n", result);
 
-    printf("plaintext:");
-    for (int i = 0; i < xlen; ++i)    printf(" %d", x[i]);
-    printf("\n\n");
+	memset(x, 0, sizeof(x));
+	start_enc = clock();
+	for (int loop_index = 0 ; loop_index < 100; loop_index++)
+	{
+		FPE_ff1_encrypt(y, x, xlen, &ff1, FPE_DECRYPT);
+	}
+	end_enc = clock();
 
-    printf("========== FF3 ==========\n");
-    FPE_ff3_encrypt(x, y, xlen, &ff3, FPE_ENCRYPT);
+	elapsed_secs = ((double)(end_enc - start_enc)) / CLOCKS_PER_SEC;
+	printf("Dec = %lf\n", elapsed_secs);
 
-    printf("ciphertext(numeral string):");
-    for (int i = 0; i < xlen; ++i)    printf(" %d", y[i]);
-    printf("\n");
+	printf("plaintext:");
+	for (int i = 0; i < xlen; ++i)    printf(" %d", x[i]);
+	printf("\n\n");
 
-    inverse_map_chars(y, result, xlen);
-    printf("ciphertext: %s\n\n", result);
+	/*   printf("========== FF3 ==========\n");
+	     FPE_ff3_encrypt(x, y, xlen, &ff3, FPE_ENCRYPT);
 
-    memset(x, 0, sizeof(x));
-    FPE_ff3_encrypt(y, x, xlen, &ff3, FPE_DECRYPT);
+	     printf("ciphertext(numeral string):");
+	     for (int i = 0; i < xlen; ++i)    printf(" %d", y[i]);
+	     printf("\n");
 
-    printf("plaintext:");
-    for (int i = 0; i < xlen; ++i)    printf(" %d", x[i]);
-    printf("\n");
+	     inverse_map_chars(y, result, xlen);
+	     printf("ciphertext: %s\n\n", result);
 
-    FPE_unset_ff1_key(&ff1);
-    FPE_unset_ff3_key(&ff3);
+	     memset(x, 0, sizeof(x));
+	     FPE_ff3_encrypt(y, x, xlen, &ff3, FPE_DECRYPT);
 
-    return 0;
+	     printf("plaintext:");
+	     for (int i = 0; i < xlen; ++i)    printf(" %d", x[i]);
+	     printf("\n");
+	 */
+	//FPE_unset_ff1_key(&ff1);
+	//FPE_unset_ff3_key(&ff3);
+
+	return 0;
 }
 
